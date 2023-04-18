8af4c8d77f62309f29b71bfed93bf27998b41966
MarazzaM <marazza.matias@gmail.com>
2023-04-17 22:42:36 -0300

    deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
-import ChatBox from '~/components/ChatBox';
-function index() {
+import Test3 from '~/components/Test3'
+import TopBar from '~/components/Topbar'
+import { Chivo_Mono } from 'next/font/google'
+
+const inter = Chivo_Mono({ subsets: ['latin'] })
+
+function ConTopBar() {
   return (
-    <div><ChatBox /></div>
+    <div className={inter.className}>
+        <TopBar />
+        <Test3 />
+    </div>
   )
 }
 
-export default index
+export default ConTopBar
